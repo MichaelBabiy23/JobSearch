4dbc670b5bb934492b1ee91406b5db5bbf77eba1
NadavRobinson <nadav832002@gmail.com>
2024-09-17 13:51:41 +0300

    added api keys cycling

--- api_handler.py
+++ api_handler.py
@@ -3,12 +3,9 @@ import json
 import os
 from tkinter import messagebox
 
-# API endpoint and default headers
-url = "https://jsearch.p.rapidapi.com/search"
-headers = {
-    "X-RapidAPI-Key": "aba64a94a8msh1c14f7ec8390ddfp1e091cjsn940fd8e59a12",  # Replace with your RapidAPI key
-    "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
-}
+api_keys = ["aba64a94a8msh1c14f7ec8390ddfp1e091cjsn940fd8e59a12", "81d662f1afmshd71bddf20a63757p13654ajsn8b46b22961d6",
+            "9d72d9cc25msh53ee03b6851a7f8p1ce3a5jsnfdc0fd11daa4"]
+counter = 0
 
 # JSON file to store query information
 json_file = "querystring.json"
@@ -31,7 +28,7 @@ def load_query_from_json():
 # Function to save collected data to JSON file
 def save_data_to_json(datastring):
     with open(data_json_file, 'w') as f:
-        json.dump(datastring, f)
+        f.write(json.dumps(datastring, indent=4))
     messagebox.showinfo("Success", "data saved to JSON file")
 
 # Function to load collected data from JSON file
@@ -43,14 +40,21 @@ def load_data_from_json():
 
 # Function to send the API request using stored querystring
 def send_request():
+    global counter
     # Load querystring from the JSON file
     querystring = load_query_from_json()
 
     if not querystring:
         messagebox.showerror("Error", "No query parameters found. Please set the parameters.")
         return
-
     # Make the API request
+    # API endpoint and default headers
+    url = "jobs-api14.p.rapidapi.com"
+    headers = {
+        "X-RapidAPI-Key": api_keys[counter],
+        "X-RapidAPI-Host": "jobs-api14.p.rapidapi.com"
+    }
+    counter = (counter + 1) % 3
     response = requests.get(url, headers=headers, params=querystring)
 
     # Check the status of the request
