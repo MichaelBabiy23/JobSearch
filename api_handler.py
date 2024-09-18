215b1324251cff81784524659983925625102996
Nadav Robinson <142407339+NadavRobinson@users.noreply.github.com>
2024-09-18 09:40:20 +0300

    storing counter to a file for better cycling

--- api_handler.py
+++ api_handler.py
@@ -6,11 +6,25 @@ from tkinter import messagebox
 api_keys = ["aba64a94a8msh1c14f7ec8390ddfp1e091cjsn940fd8e59a12",
             "81d662f1afmshd71bddf20a63757p13654ajsn8b46b22961d6",
             "9d72d9cc25msh53ee03b6851a7f8p1ce3a5jsnfdc0fd11daa4"]
-counter = 0
 
 # JSON file to store query information
 json_file = "querystring.json"
 data_json_file = "data.json"
+counter_file = "counter.txt"
+
+# Function to save counter to txt file
+def save_counter(counter):
+    with open(counter_file, 'w') as f:
+        f.write(str(counter))
+
+# Function to load counter from txt file
+def load_counter():
+    if os.path.exists(counter_file):
+        with open(counter_file, 'r') as f:
+            return int(f.read())
+    return 0
+
+counter = load_counter()
 
 # Function to save querystring to JSON file
 def save_query_to_json(querystring):
@@ -23,7 +37,7 @@ def load_query_from_json():
     if os.path.exists(json_file):
         with open(json_file, 'r') as f:
             return json.load(f)
-    return {}  # Return empty dictionary if file does not exist
+    return {}  # Return.exists empty dictionary if file does not exist
 
 
 # Function to save collected data to JSON file
@@ -48,6 +62,7 @@ def send_request():
     if not querystring:
         messagebox.showerror("Error", "No query parameters found. Please set the parameters.")
         return
+    
     # Make the API request
     # API endpoint and default headers
     url = "https://jobs-api14.p.rapidapi.com/list"
@@ -55,7 +70,10 @@ def send_request():
         "X-RapidAPI-Key": api_keys[counter],
         "X-RapidAPI-Host": "jobs-api14.p.rapidapi.com"
     }
+
     counter = (counter + 1) % 3
+    save_counter(counter)
+
     response = requests.get(url, headers=headers, params=querystring)
 
     # Check the status of the request
