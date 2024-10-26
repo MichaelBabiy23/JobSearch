d7022ab15f6b76d03911197cfbb000e62a1950de
MichaelBabiy23 <mr.babiymichael@gmail.com>
2024-10-26 15:43:47 +0300

    removed tkinter lib for hosting

--- json_file_funcs.py
+++ json_file_funcs.py
@@ -1,6 +1,5 @@
 import json
 import os
-from tkinter import messagebox
 
 
 json_file = "querystring.json"
@@ -13,7 +12,7 @@ FILE_PATH = "responses.json"
 def save_query_to_json(querystring):
     with open(json_file, 'w') as f:
         json.dump(querystring, f)
-    messagebox.showinfo("Success", "Query parameters saved to JSON file")
+    # messagebox.showinfo("Success", "Query parameters saved to JSON file")
 
 
 # Function to load querystring from JSON file
@@ -28,7 +27,7 @@ def load_query_from_json():
 def save_data_to_json(data_string):
     with open(data_json_file, 'w') as f:
         f.write(json.dumps(data_string, indent=4))
-    messagebox.showinfo("Success", "data saved to JSON file")
+    # messagebox.showinfo("Success", "data saved to JSON file")
 
 
 # Function to load collected data from JSON file
